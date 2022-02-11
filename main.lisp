eb8447e70f732d984b98beb2752365707484efab
larissa.gremelmaier.rosa <larissa.gremelmaier.rosa@grad.ufsc.br>
2022-02-11 19:39:34 -0300

    backtracking v0

--- main.lisp
+++ main.lisp
@@ -127,8 +127,43 @@
     
 )
 
-(defun main()
-    (setf mp(list '(3 0 0 3 1 2 0)
+(defun backtrack (v x y mp ma s)
+  (if (or (< x 0) (< y 0))
+      (return-from backtrack NIL)
+      (if (and ( > x (- (compMatrix mp) 1))  (> y (- (compMatrix mp) 1)))
+          (return-from backtrack (geraResultado mp (reverse s) 0 0))
+          (if (/= 0 (nth y (nth x mp)))
+            (if (<  (+ y 1) (compMatrix mp))
+                (return-from backtrack (backtrack 1 x (+ y 1) mp ma s))
+                (if (>= (+ x 1) (compMatrix mp))
+                    (return-from backtrack (backtrack 1 (+ x 1) (+ y 1) mp ma s))
+                    (return-from backtrack (backtrack 1 (+ x 1) 0 mp ma s))
+                )
+            )
+            (if (> v (tamArea ma (nth y (nth x mp)) ))
+              (return-from backtrack (backtrack ((car s) + 1) (espacoembranco mp x y) mp ma (cdr s)))
+              (if ((verify (geraResultado mp (reverse s) 0 0) ma v 0 0))
+                (if ( < (+ y 1) (compMatrix mp))
+                    (return-from backtrack (backtrack 1 x (+ y 1) mp ma (cons v s)))
+                    (if (>= (+ x 1) (compMatrix mp))
+                      (return-from backtrack (backtrack 1 (+ x 1) (+ y 1) mp ma (cons v s)))
+                      (return-from backtrack (backtrack 1 (+ x 1) 0 mp ma (cons v s)))
+                    )
+                )
+                (return-from backtrack (backtrack (+ v 1) x y mp ma s))
+              )
+            )
+
+          )
+      )
+
+  )
+
+)
+
+
+
+(setf mp(list '(3 0 0 3 1 2 0)
                '(0 6 0 4 0 0 3)
                '(2 0 0 0 3 2 0)
                '(0 5 0 0 0 0 0)
@@ -136,9 +171,9 @@
                '(0 0 1 5 1 0 1)
                '(1 0 0 2 0 3 4)
             )
-    )
+)
 
-    (setf ma(list '(1 1 2 2 2 5 5)
+(setf ma(list '(1 1 2 2 2 5 5)
             '(1 1 3 4 4 6 5)
             '(1 1 4 4 8 6 6)
             '(7 4 4 8 8 9 6)
@@ -146,7 +181,8 @@
             '(10 10 10 9 9 9 12)
             '(11 10 10 12 12 12 12)
         )
-    )
 )
 
-main()
+(print(backtrack 1 0 0 mp ma (list NIL)))
+
+
