c2cdd8a8cea365ec31f3af6a116d6f9a9644b4d7
larissa.gremelmaier.rosa <larissa.gremelmaier.rosa@grad.ufsc.br>
2022-02-11 18:31:21 -0300

    geraResultado fixed

--- geraResultado.lisp
+++ geraResultado.lisp
@@ -1,23 +1,41 @@
-(defun geraResultado (listA listB x y)
-    (setf sublist (nth x listA))
-    (if (null listB)
-        listA
-        (if ((and (= y (- 1 compMatrix listA)) (= 0 (nth y sublist))))   
-            geraResultado (insertMatrix listA x y (car listB)) (cdr listB) (+ x 1) 0
-            (if (= 0 (nth y sublist))
-                geraResultado (insertMatrix listA x y (car listB)) (cdr listB) x (+ y 1)
-                (if ((and (= y (- 1 compMatrix listA)) (/= 0 (nth y sublist))))
-                    geraResultado listA listB (+ x 1) 0
-                    geraResultado listA listB x (+ y 1)
-                )
+(defun compMatrix(matriz)
+  (if (null matriz)
+    0
+    (+ 1 (compMatrix (cdr matriz)))))
+
+(defun insertList(l x v)
+  (setf (nth x l) v)
+  l)
+
+(defun insertMatrix(m x y v)
+  (setf (nth x m) (insertList (nth x m) y v))
+  m)
+
+(defun geraResultado (mp listaP x y)
+
+    (if (null listaP)
+        (return-from geraResultado mp)
+        (if (and (= 0 (nth y (nth x mp))) (= y (- (compMatrix mp) 1)))
+            (return-from geraResultado (geraResultado (insertMatrix mp x y (car listaP)) (cdr listaP) (+ x 1) 0))
+            (if (= 0 (nth y (nth x mp)))
+                 (return-from geraResultado (geraResultado (insertMatrix mp x y (car listaP)) (cdr listaP) x (+  y 1)))
+                 (if (and (/= 0 (nth y (nth x mp))) (= y (- (compMatrix mp) 1)))
+                    (return-from geraResultado (geraResultado mp listaP (+ x 1) 0))
+                    (return-from geraResultado (geraResultado mp listaP x (+ y 1)))
+                 )
+            
             )
         )
     )
+    
 )
-#|      LINHAS 5 E 9 PRECISAM SER TESTADAS POIS GERA O SEGUINTE AVISO
 
-     SYSTEM::%EXPAND-FORM:
-          (AND (= Y (- 1 COMPMATRIX LISTA)) (= 0 (NTH Y SUBLIST))) should be a
-          lambda expression
 
-|#
+(print (geraResultado (list '(1 1 0 2 0 5 5)
+          '(1 1 3 4 4 6 5)
+          '(1 1 4 4 0 6 6)
+          '(7 4 4 8 8 9 6)
+          '(7 7 8 8 9 9 12)
+          '(10 10 10 9 9 9 12)
+          '(11 10 10 12 12 12 12)
+       ) (list 9 3 6) 0 0))
